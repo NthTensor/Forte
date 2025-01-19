dd62c1e6fc6ffb039f2d3a7ef521ce204140890e
Miles Silberling-Cook <nth.tensor@gmail.com>
2025-01-18 21:32:38 -0500

    Add loom to CI

---
+++ ci/src/commands/loom_test.rs
@@ -0,0 +1,20 @@
+use crate::{Flag, Prepare, PreparedCommand};
+use argh::FromArgs;
+use xshell::cmd;
+
+/// Runs the loom concurrency test suite.
+#[derive(FromArgs, Default)]
+#[argh(subcommand, name = "loom-test")]
+pub struct LoomTestCommand {}
+
+impl Prepare for LoomTestCommand {
+    fn prepare<'a>(&self, sh: &'a xshell::Shell, _flags: Flag) -> Vec<PreparedCommand<'a>> {
+        let command = PreparedCommand::new::<Self>(
+            cmd!(sh, "cargo test --test loom --release"),
+            "Please fix compiler errors in output above.",
+        )
+        .with_env_var("RUSTFLAGS", "--cfg loom")
+        .with_env_var("LOOM_MAX_PREEMPTIONS", "5");
+        vec![command]
+    }
+}
